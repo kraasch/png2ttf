2831d5c899a8fb009aea16c1a559bd876fca7f0b
Alex Kraasch <alex@kraasch.eu>
2023-10-05 16:34:32 +0200

    Update todos for last conversion script.

--- 03_python_svg-to-ttf/svg2ttf.py
+++ 03_python_svg-to-ttf/svg2ttf.py
@@ -1,4 +1,7 @@
 
 import os
 
-os.system('cmd /k "fontforge -lang=py -script svgs2ttf examples/example.json"')
+# TODO: use some command on windows here (make use of fontforge's python abilities).
+# os.system('cmd /k "fontforge -lang=py -script svgs2ttf examples/example.json"')
+
+#TODO: use another command on linux here (make use of fontforge's python abilities).
