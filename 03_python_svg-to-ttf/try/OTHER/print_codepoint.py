4b97c35444ecb2b3557ae454b887bb548dc90821
Alex Kraasch <alex@kraasch.eu>
2023-10-05 21:50:04 +0200

    Courtesy flush.

--- 03_python_svg-to-ttf/try/OTHER/print_codepoint.py
+++ 03_python_svg-to-ttf/try/OTHER/print_codepoint.py
@@ -7,4 +7,5 @@ for c in cs:
     print(f"{c}", end='\t')
     word=ord(c.encode('unicode_escape'))
     print(word, end='\t')
-    print(format(word, '#08x'))
+    print(format(word, '#04x'), end='\t')
+    print()
