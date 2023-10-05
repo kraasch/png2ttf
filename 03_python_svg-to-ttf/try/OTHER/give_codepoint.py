2071d8bf4ee58481337eb399046e79612c0bf7e3
Alex Kraasch <alex@kraasch.eu>
2023-10-05 21:22:09 +0200

    Courtesy flush.

---
+++ 03_python_svg-to-ttf/try/OTHER/give_codepoint.py
@@ -0,0 +1,14 @@
+
+import sys
+import os
+
+input_file=sys.argv[1]
+
+with open(input_file) as f:
+    while True:
+        c = f.read(1)
+        if not c:
+            break
+        #if not c == os.linesep:
+        if not c == '\n':
+            print(f"{c}")
