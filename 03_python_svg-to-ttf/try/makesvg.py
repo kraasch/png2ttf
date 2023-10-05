4dee4f25da5da243a6c8f50c4aa606655c6a91f6
Alex Kraasch <alex@kraasch.eu>
2023-10-05 21:04:21 +0200

    Courtesy flush.

--- 03_python_svg-to-ttf/try/makesvg.py
+++ 03_python_svg-to-ttf/try/makesvg.py
@@ -1,5 +1,10 @@
 #!/usr/bin/fontforge -script
 
+#import os
+# TODO: use some command on windows here (make use of fontforge's python abilities).
+# os.system('cmd /k "fontforge -lang=py -script svgs2ttf examples/example.json"')
+# TODO: use another command on linux here (make use of fontforge's python abilities).
+
 import sys
 import os.path
 import json
--- 03_python_svg-to-ttf/try/svg2ttf.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-# TODO: use some command on windows here (make use of fontforge's python abilities).
-# os.system('cmd /k "fontforge -lang=py -script svgs2ttf examples/example.json"')
-
-# TODO: use another command on linux here (make use of fontforge's python abilities).
